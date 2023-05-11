d963b6163632785f7bc230c46aba0f8f45472731
Perspirator <53982052+Perspirator@users.noreply.github.com>
2023-05-11 01:57:00 -0700

    Update MapQuest_112.py

--- MapQuest_112.py
+++ MapQuest_112.py
@@ -6,14 +6,14 @@ from AI import * #TODO: Gaurav
 # takes in app object; initiates app properties
 def appStarted(app):
 
-    
+    #initialze game properties
     app.gold0 = 10
     app.gold1 = 10
     app.turn = 0
     app.timerDelay = 500
     app.gameState = 0
 
-    #board
+    # set up game board
     app.board = generateMap(6)
     app.visibility = [([0] * 6) for i in range(6)]
     app.visibility[0][0] = 1
@@ -21,12 +21,13 @@ def appStarted(app):
     app.visibility[1][0] = 1
     app.visibility[1][1] = 1
 
-    
+    # initialize game characters and selection options
     app.characters = []
     app.selected = None
     app.createable = 0
     app.moveable = []
 
+    # set up game modes and dimensions
     app.mode = 'titleScreen'
     app.border = 30
     app.squareLength = (app.height - 2 * app.border) // 6 #len(app.board)
@@ -94,10 +95,12 @@ def appStarted(app):
 
 #take in app; start timer
 def gameMode_timerFired(app):
+    # check if the game has been won or lost and update the mode accordingly
     if app.gameState == 1:
         app.mode = "winScreen"
     elif app.gameState == 2:
         app.mode = "loseScreen"
+        # if it is player 2's turn, update their gold and moves allowed and switch turns to player 1
     elif app.turn == 1:
         getGold(app)
         getMove(app)
@@ -105,11 +108,12 @@ def gameMode_timerFired(app):
 
 #take in app; augment gold per each sides' gold farm
 def getGold(app):
+    # update player 1's gold
     app.gold0 += 1
     for i in app.board:
             app.gold0 += i.count('FarmKosbie')
     
-
+    # update player 2's gold
     app.gold1 += 1
     for i in app.board:
             app.gold1 += i.count('FarmTaylor')
@@ -120,21 +124,24 @@ def gameMode_mousePressed(app,event):
     options_center = (2 * app.border +  app.squareLength * len(app.board))
     options_center = options_center + (app.width - options_center) // 2 
     
-    #click help menu
+    #check if help menu was clicked
     if (event.x > options_center - 100 and event.x < options_center + 100 and
         event.y > 620 and event.y < 680):
         app.mode = "helpScreen"
-    
+    # check if it is player 1's turn
     if app.turn == 0:
+        # check if the click was within the game board
         if (row >= 0 and row < len(app.board) and
             col >= 0 and col < len(app.board)):
 
             #check if there is a piece selected
             if app.selected != None and type(app.selected) != tuple and app.selected.owner == 'Kosbie':
+                # check if the selected piece can move to the clicked location
                 if (row,col) in app.moveable:
+                    # move the piece to the clicked location and switch turns to player 2
                     app.selected.move(app,row,col)
                     app.turn = 1 - app.turn
-                
+                # clear the selection if a valid move was not made
                 clearSelection(app)
 
             #if nothing is selected, then tries to select a piece or structure
@@ -142,6 +149,7 @@ def gameMode_mousePressed(app,event):
                 #check if piece there
                 soldier = soldierSelected(app,row,col)
                 if soldier != None and soldier.owner == 'Kosbie':
+                    # select the soldier and generate its allowed moves
                     app.selected = soldier
                     app.moveable = soldier.generateMovesAllowed(app)
                 
@@ -175,10 +183,12 @@ def gameMode_mousePressed(app,event):
             clearSelection(app)
 #input: app; delete selected item in-game
 def clearSelection(app):
+    # clear the selected item and reset the moveable and createable options
     app.selected = None
     app.moveable = []
     app.createable = 0
 #
+# check how many characters can be created based on the player's gold
 def generateCreateable(app):
     if app.gold0 >= 9:
         return 3
@@ -192,18 +202,22 @@ def generateCreateable(app):
 
 #inputs: app, dim - set value of dimmness; dims borders of game
 def getDim(app,dim):
+     # calculate the row or column based on the given dimension
     return (dim - app.border) // app.squareLength
 
 #inputs: app, row - x-coor, col - y-coor; select soldier; return selected soldier
 def soldierSelected(app,row,col):
+    # check if there is a soldier at the given row and column and return it if found
     for character in app.characters:
         if character.x == col and character.y == row:
             return character
+        # return None if no soldier was found at the given location
     return None
 
         
 #inputs: app, canvas - drawable space; iniate all visual elements    
 def gameMode_redrawAll(app,canvas):
+    # draw the game board and all visual elements
     canvas.create_rectangle(0,0,800,800, fill = 'blue')
     drawBoard(app,canvas)
     drawCharacters(app,canvas)
@@ -213,6 +227,7 @@ def gameMode_redrawAll(app,canvas):
 
 #inputs: app, canvas - drawable space; draw cloud background
 def drawFog(app, canvas):
+    # draw the fog of war on the game board based on the visibility matrix
     for row in range(len(app.visibility)):
         for col in range(len(app.visibility)):
             if app.visibility[col][row] % 2 != 1:
@@ -222,6 +237,7 @@ def drawFog(app, canvas):
 
 #inputs: app, canvas - drawable space; illustrate possible moves to player
 def drawMovesAllowed(app,canvas):
+    # draw circles on the game board to indicate where a selected piece can move to
     for (row,col) in app.moveable:
         circleCenterX = col * app.squareLength + app.border + app.squareLength // 2
         circleCenterY = row * app.squareLength + app.border + app.squareLength //2
@@ -231,11 +247,13 @@ def drawMovesAllowed(app,canvas):
 
 #inputs: app, canvas - drawable space; draw user-interface for player to access settings
 def drawCreateOptions(app,canvas):
+    # calculate the center of the options menu
     options_center = (2 * app.border +  app.squareLength * len(app.board))
     options_center = options_center + (app.width - options_center) // 2 
-
+    
+    # draw the create menu text
     canvas.create_text(options_center, 50, text = "Create Menu", font = 'Arial 40 underline', fill='black')
-
+    # draw the createable character options based on the player's gold
     if app.createable > 2:
         canvas.create_rectangle(options_center - 150, 280, options_center + 150, 340)
         canvas.create_text(options_center, 310, text = 'Knight\t\t9G', font = 'Arial 20', fill='gray')
@@ -248,7 +266,7 @@ def drawCreateOptions(app,canvas):
         canvas.create_rectangle(options_center - 150, 120, options_center + 150, 180)
         canvas.create_text(options_center, 150, text = 'Soldier\t\t4G', font = 'Arial 20', fill='gray')
     
-
+    # draw the help menu button and player information
     canvas.create_rectangle(options_center - 100, 620, options_center + 100, 680, fill = 'gray')
     canvas.create_text(options_center, 650, text = 'Help', font = 'Arial 30')
     canvas.create_text(options_center, 500, text = f'Player Gold: {app.gold0}', font = 'Arial 30', fill='gray')
@@ -261,6 +279,7 @@ def drawCreateOptions(app,canvas):
         
 #inputs: app, canvas - drawable space; illustrate characters
 def drawCharacters(app,canvas):
+    # draw the characters on the game board based on their type and owner
     for character in app.characters:
         if type(character) == Archer:
             if character.owner == 'Kosbie':
@@ -295,6 +314,7 @@ def drawCharacters(app,canvas):
 
 #draws images for each terrain
 def drawBoard(app,canvas):
+    # draw the game board based on the board matrix
     for row in range(len(app.board)):
         for col in range(len(app.board)):
             if app.board[col][row] == 'Grass':
@@ -359,14 +379,17 @@ def drawBoard(app,canvas):
 
 #inputs: app, canvas - drawable space; indicate win for player
 def winScreen_redrawAll(app,canvas):
+    # draw the win screen image
     canvas.create_image(app.width//2, app.height//2,
                         image=ImageTk.PhotoImage(app.win_screen))
 #inputs: app, canvas - drawable space; indicate lose for player
 def loseScreen_redrawAll(app,canvas):
+    # draw the lose screen image
     canvas.create_image(app.width//2, app.height//2,
                         image=ImageTk.PhotoImage(app.lose_screen))
 #inputs: app, canvas - drawable space; show interface for help with game
 def helpScreen_redrawAll(app,canvas):
+    # draw the help screen image and return button
     canvas.create_image(app.width//2, app.height//2 - 50,
                         image=ImageTk.PhotoImage(app.help_screen))
     canvas.create_rectangle(app.width // 2 - 100, app.height // 2 + 265, app.width // 2 + 100, 
@@ -374,12 +397,14 @@ def helpScreen_redrawAll(app,canvas):
     canvas.create_text(app.width//2, app.height//2 + 300, text = "Return", font = 'Arial 40', fill = 'white')
 #inputs: app, canvas - drawable space; use mouse to interface with help screen
 def helpScreen_mousePressed(app,event):
+    # check if the return button was clicked and switch back to the game mode if it was
     if (event.x > app.width // 2 - 100 and event.x < app.width // 2 + 100 and
         event.y > app.height // 2 + 265 and event.y < app.height // 2 + 335):
             app.mode = 'gameMode'
 
 #inputs: app, canvas - drawable space; redraw titlescreen 
 def titleScreen_redrawAll(app,canvas):
+    # draw the title screen background and start button
     canvas.create_image(app.width//2, app.height//2,
                         image=ImageTk.PhotoImage(app.game_background))\
 
@@ -392,12 +417,14 @@ def titleScreen_redrawAll(app,canvas):
     
 #inputs: app, canvas - drawable space; use mouse input to interface with title-screen
 def titleScreen_mousePressed(app,event):
+    # check if the start button was clicked and switch to the lore screen if it was
     if (event.x > app.width // 2 - 100 and event.x < app.width // 2 + 100 and
         event.y > app.height // 2 + 165 and event.y < app.height // 2 + 235):
             app.mode = 'LoreScreen'
 
 #inputs: app, canvas - drawable space; display lore 
 def LoreScreen_redrawAll(app,canvas):
+    # draw the lore screen image and next button
     canvas.create_image(app.width//2, app.height//2 - 75,
                         image=ImageTk.PhotoImage(app.game_lore))
     
@@ -406,6 +433,7 @@ def LoreScreen_redrawAll(app,canvas):
     canvas.create_text(app.width//2, app.height//2 + 295, text = "Next", font = 'Arial 40', fill = 'white')
 #inputs: app, canvas - drawable space; use mouse input to interface with lore-screen
 def LoreScreen_mousePressed(app,event):
+    # check if the next button was clicked and switch to the game mode if it was
     if (event.x > app.width // 2 - 100 and event.x < app.width // 2 + 100 and
         event.y > app.height // 2 + 260 and event.y < app.height // 2 + 330):
             app.mode = 'gameMode'
