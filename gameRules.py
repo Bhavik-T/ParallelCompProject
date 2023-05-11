c6d0db5cd5b1117f69918421e7047dd0a24371f5
Perspirator <53982052+Perspirator@users.noreply.github.com>
2023-05-11 12:12:02 -0700

    Update gameRules.py

--- gameRules.py
+++ gameRules.py
@@ -21,13 +21,14 @@ from collections import deque
 def checkFairness(board):
     numOutpost = 0
     numFarm = 0
+    # Count the number of outposts and farms on the board
     for i in range(len(board)):
         for j in range(len(board)):
             if board[i][j] == 'Farm':
                 numFarm += 1
             elif board[i][j] == 'Outpost':
                 numOutpost += 1
-    
+    # Check if the number of outposts and farms is within the valid range
     if numOutpost < 2 or numOutpost > 5 or numFarm < 2 or numFarm > 5:
         return False
     return True
@@ -37,7 +38,8 @@ def checkFairness(board):
 def checkMap(board):
     if checkFairness(board) == False:
         return False
-    
+    # Create a matrix to represent the board, where bad squares (ocean and mountain) are assigned a large value
+    # and valid squares are assigned a small value
     badSquares = ['Ocean', 'Mountain']
     matrix = []
     validSquares = []
@@ -49,13 +51,14 @@ def checkMap(board):
             else:
                 matrix[i].append(1)
                 validSquares.append((i, j))
-    
+    # Check if there exists a path between every pair of valid squares on the board using BFS
     for i in range(len(validSquares)):
         for j in range(i, len(validSquares)):
             start = validSquares[i]
             end = validSquares[j]
             if BFS(start, end, matrix) == -1:
                 return False
+    # Check if the game is fair again
     if checkFairness(board) == False:
         return False
 
@@ -68,10 +71,12 @@ def BFS(start, end, graph):
     while queue:
         path = queue.pop(0)
         if len(path) > 20:
+            # limit the maximum length of a path to 20 squares to prevent an infinite loop
             return False
         x, y = path[-1]
         if x == end[0] and y == end[1]:
             return True
+        # Check the four adjacent squares of the current square and add them to the queue if they are valid
         for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
             if 0 <= x2 < len(graph) and 0 <= y2 < len(graph) and graph[x2][y2] != 120 and (x2, y2) not in seen:
                 queue.append(path + [(x2, y2)])
@@ -83,6 +88,7 @@ def BFS(start, end, graph):
 #generate map with images given dimensions
 def generateMap(dimensions):
     
+    # Initialize the board with grass in all cells
     board = [
         ['Grass' for i in range(dimensions)] for j in range(dimensions)
     ]
@@ -90,6 +96,7 @@ def generateMap(dimensions):
 
     #Name region
 
+    # Define the names for different regions
     baseName = 'Base'
     grassName = 'Grass'
     mountainName = 'Mountain'
@@ -100,6 +107,7 @@ def generateMap(dimensions):
     outpostName = 'Outpost'
     farmName = 'Farm'
 
+    # Define the names for specific outposts and farms
     kosbieOutpostName = outpostName + kosbieName
     taylorOutpostName = outpostName + taylorName
 
@@ -117,17 +125,22 @@ def generateMap(dimensions):
     probMountain = 70                            #Mountains are one square types, so their probabilities are not cumulative.
 
     while True:
+        # Iterate through each cell in the board
         for x in range(dimensions):
             for y in range(dimensions):
                 cell = board[x][y]
+                
+                # If the cell is not grass, skip it (e.g. if it's already occupied by an ocean)
                 if cell != grassName:                  #Taken, typically by an ocean as oceans exist as blocks.
                     continue
+                # Generate random values to decide the type of the square
                 randVal1 = random.randint(0, 1000)        #A random integer to decide the position of the square.
                 randVal2 = random.randint(0, 1000)
                 randVal3 = random.randint(0, 1000)
                 randVal4 = random.randint(0, 1000)
+                # Check if the cell is on the border of the map
                 isBorder = isExterior(x, y, dimensions)    
-
+                # Set the probabilities of placing outposts and farms based on whether the cell is on the border or not
                 if isBorder == True:
                     probOutpost = probOutpostInterior
                     probFarm = probFarmInterior
@@ -135,7 +148,7 @@ def generateMap(dimensions):
                 else:
                     probOutpost = probOutpostExterior
                     probFarm = probFarmExterior
-                
+                # Check the random values against the probabilities and update the cell type accordingly
                 if randVal1 < probOcean:
                     board[x][y] = 'Ocean'
                     continue
@@ -153,12 +166,13 @@ def generateMap(dimensions):
                 if randVal4 < probFarm:
                     board[x][y] = 'Farm'
                     continue
-        
+        # Set the locations of the two bases
         board[0][0] = 'BaseKosbie'
         board[-1][-1] = 'BaseTaylor'
-
+        # Check if the map is valid (e.g. both bases are present)
         if checkMap(board):
             return board
+        # If the map is invalid, start over with a new board
         board = [
             ['Grass' for i in range(dimensions)] for j in range(dimensions)
         ]
